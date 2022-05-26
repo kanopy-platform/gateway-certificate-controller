48e7126e8a3ddd27012d97c59783a709f7bc5734
Yuzhou Liu <yuzhou.liu@mongodb.com>
2022-05-26 16:33:50 -0400

    remoe unused code

--- pkg/v1beta1/labels/labels_test.go
+++ pkg/v1beta1/labels/labels_test.go
@@ -6,11 +6,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestBuildLabel(t *testing.T) {
-	t.Parallel()
-	assert.Equal(t, "v1beta1.kanopy-platform.github.io/istio-cert-controller-inject-simple-credential-name", InjectSimpleCredentialNameLabel())
-}
-
 func TestBuildLabelSelector(t *testing.T) {
 	t.Parallel()
 	assert.Equal(t, "v1beta1.kanopy-platform.github.io/istio-cert-controller-inject-simple-credential-name=true", InjectSimpleCredentialNameLabelSelector())
