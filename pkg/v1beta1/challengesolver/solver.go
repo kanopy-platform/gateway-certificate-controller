5defca6dae25df12797feffcb82d6755304963c0
wwalter <53450727+whwalter@users.noreply.github.com>
2023-09-07 16:22:03 -0400

    rough challenge solver

--- pkg/v1beta1/challengesolver/solver.go
+++ pkg/v1beta1/challengesolver/solver.go
@@ -6,79 +6,124 @@ import (
 	"hash/adler32"
 
 	acmev1 "github.com/cert-manager/cert-manager/pkg/apis/acme/v1"
+	acmev1Client "github.com/cert-manager/cert-manager/pkg/client/clientset/versioned/typed/acme/v1"
+
 	"github.com/kanopy-platform/gateway-certificate-controller/pkg/v1beta1/cache"
-	networkingv1beta1 "istio.io/client-go/pkg/clientset/versioned/typed/networking/v1beta1"
+	networkingv1beta1 "istio.io/client-go/pkg/apis/networking/v1beta1"
+	networkingv1beta1Client "istio.io/client-go/pkg/clientset/versioned/typed/networking/v1beta1"
 
 	istiov1beta1 "istio.io/api/networking/v1beta1"
 
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	corev1 "k8s.io/client-go/kubernetes/typed/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/yaml"
 )
 
 type ChallengeSolver struct {
-	coreClient       corev1.Corev1Interface
-	networkingClient networkingv1beta1.NetworkingV1beta1Interface
+	coreClient       corev1.CoreV1Interface
+	networkingClient networkingv1beta1Client.NetworkingV1beta1Interface
+	acmeClient       acmev1Client.AcmeV1Interface
 	glc              *cache.GatewayLookupCache
 }
 
-func (cs *ChallengeSolver) Reconcile(ctx context.Context, o Request) (Result, error) {
-
-	return nil, nil
+func NewChallengeSolver(cc corev1.CoreV1Interface, nc networkingv1beta1Client.NetworkingV1beta1Interface, ac acmev1Client.AcmeV1Interface, glc *cache.GatewayLookupCache, opts ...OptionsFunc) *ChallengeSolver {
+	return &ChallengeSolver{
+		coreClient:       cc,
+		networkingClient: nc,
+		acmeClient:       ac,
+		glc:              glc,
+	}
 }
 
-func (cs *ChallengeSolver) Solve(challenge *acmev1.Challenge) {
+func (cs *ChallengeSolver) Reconcile(ctx context.Context, req reconcile.Request) (reconcile.Result, error) {
+
+	log := log.FromContext(ctx)
+	log.Info("Reconciling Acme Challenge", "reconcile", req.String())
+	log.V(1).Info("Debug")
+
+	challenge, err := cs.acmeClient.Challenges(req.Namespace).Get(ctx, req.Name, metav1.GetOptions{})
+	if err != nil {
+		if errors.IsNotFound(err) {
+			return reconcile.Result{}, nil // garbage collection will handle
+		}
+
+		log.Error(err, "Error reconciling challenge, requeued")
+		return reconcile.Result{
+			Requeue: true,
+		}, err
+	}
+
+	err = cs.Solve(ctx, challenge)
+	if err != nil {
+		//TODO type errors as recoverable or not
+		return reconcile.Result{
+			Requeue: true,
+		}, err
+	}
+
+	return reconcile.Result{}, nil
+}
+
+func (cs *ChallengeSolver) Solve(ctx context.Context, challenge *acmev1.Challenge) error {
+	log := log.FromContext(ctx)
+	log.V(1).Info("Debug")
 
 	httpDomainHash := fmt.Sprintf("%d", adler32.Checksum([]byte(challenge.Spec.DNSName)))
 	tokenHash := fmt.Sprintf("%d", adler32.Checksum([]byte(challenge.Spec.Token)))
 
-	namespacedGateway, ok := glc.Get(challenge.Spec.DNSName)
+	namespacedGateway, ok := cs.glc.Get(challenge.Spec.DNSName)
 	if !ok {
 		// requeue the request to wait for the lookup cache to populate
 		// probably needs backoff
-		return
+		return fmt.Errorf("Host %s: Gateway not found.", challenge.Spec.DNSName)
 	}
+	log.V(1).Info(fmt.Sprintf("Debug: gateway found %s", namespacedGateway))
 
 	listOpts := metav1.ListOptions{
-		LabelSelector: fmt.Sprinf("%s=%s,%s=%s", acmev1.DomainLabelKey, httpDomainHash, acmev1.TokenLabelKey, tokenHash),
+		LabelSelector: fmt.Sprintf("%s=%s,%s=%s", acmev1.DomainLabelKey, httpDomainHash, acmev1.TokenLabelKey, tokenHash),
 	}
 
-	serviceList, err := cs.coreClient.Services(challenge.Namespace).List(context.TODO, listOpts)
+	serviceList, err := cs.coreClient.Services(challenge.Namespace).List(context.TODO(), listOpts)
 	if err != nil {
 		// requeue the request to wait for the service to appear in the api
-		return
+		return err
 	}
 
 	if len(serviceList.Items) == 0 {
 		// requeue the request to wait for the service to appear in the api
-		return
+		return fmt.Errorf("No service matched selector: %s", fmt.Sprintf("%s=%s,%s=%s", acmev1.DomainLabelKey, httpDomainHash, acmev1.TokenLabelKey, tokenHash))
 	}
 	svc := serviceList.Items[0]
 
 	if len(svc.Spec.Ports) == 0 {
 		// this is probably unrecoverable
-		return
+		return fmt.Errorf("Service: %s, missing port definition", svc.Name)
 	}
-	port := uint(svc.Spec.Ports[0].Port)
+	port := uint32(svc.Spec.Ports[0].Port)
 	vs := networkingv1beta1.VirtualService{
 		Spec: istiov1beta1.VirtualService{
-			Hosts:    []string{challenge.Spec.DNSNam},
+			Hosts:    []string{challenge.Spec.DNSName},
 			Gateways: []string{namespacedGateway},
 			Http: []*istiov1beta1.HTTPRoute{
 				{
 					Name: "solver",
 					Match: []*istiov1beta1.HTTPMatchRequest{
 						{
-							Uri: *istiov1beta1.StringMatch{
-								MatchType: StringMatch_Exact{
-									Exact: fmt.Sprintf("/.well-known/acme-challenge/%s", challenge.Token),
+							Uri: &istiov1beta1.StringMatch{
+								MatchType: &istiov1beta1.StringMatch_Exact{
+									Exact: fmt.Sprintf("/.well-known/acme-challenge/%s", challenge.Spec.Token),
 								},
 							},
 						},
 					},
 					Route: []*istiov1beta1.HTTPRouteDestination{
 						{
-							Destination: *istiov1beta1.Destination{
+							Destination: &istiov1beta1.Destination{
 								Host: svc.Name,
-								Port: *istiov1beta1.PortSelector{
+								Port: &istiov1beta1.PortSelector{
 									Number: port,
 								},
 							},
@@ -91,18 +136,21 @@ func (cs *ChallengeSolver) Solve(challenge *acmev1.Challenge) {
 
 	vs.Name = fmt.Sprintf("%s", challenge.Name)
 	vs.Namespace = challenge.Namespace
-	vs.ObjectMeta.OwnerReferences[0] = metav1.OwnerReference{
+	vs.ObjectMeta.OwnerReferences = append(vs.ObjectMeta.OwnerReferences, metav1.OwnerReference{
 		APIVersion: acmev1.SchemeGroupVersion.String(),
 		Kind:       "Challenge",
 		Name:       challenge.Name,
 		UID:        challenge.UID,
-	}
+	})
 
-	_, err := cs.networkingClient.VirtualService(challenge.Namespace).Create(context.TODO, &vs, metav1.CreateOptions)
+	vsb, _ := yaml.Marshal(vs)
+	fmt.Println(string(vsb))
+
+	_, err = cs.networkingClient.VirtualServices(challenge.Namespace).Create(context.TODO(), &vs, metav1.CreateOptions{})
 	if err != nil {
 		// requeue the request to wait for the service to appear in the api
-		return
+		return err
 
 	}
-	return
+	return nil
 }
--- pkg/v1beta1/challengesolver/solver_test.go
+++ pkg/v1beta1/challengesolver/solver_test.go
@@ -1,14 +1,30 @@
 package challengesolver_test
 
 import (
+	"context"
+	"fmt"
+	"hash/adler32"
 	"testing"
 
+	acmev1 "github.com/cert-manager/cert-manager/pkg/apis/acme/v1"
 	"github.com/kanopy-platform/gateway-certificate-controller/pkg/v1beta1/cache"
+	"github.com/kanopy-platform/gateway-certificate-controller/pkg/v1beta1/challengesolver"
+	"go.uber.org/zap/zapcore"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	"github.com/stretchr/testify/assert"
 
 	certmanagerfake "github.com/cert-manager/cert-manager/pkg/client/clientset/versioned/fake"
+	acmefake "github.com/cert-manager/cert-manager/pkg/client/clientset/versioned/typed/acme/v1/fake"
 	istiofake "istio.io/client-go/pkg/clientset/versioned/fake"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	k8sfake "k8s.io/client-go/kubernetes/fake"
+	corev1fake "k8s.io/client-go/kubernetes/typed/core/v1/fake"
+	k8stesting "k8s.io/client-go/testing"
+	"k8s.io/klog/v2"
 )
 
 func TestStub(t *testing.T) {
@@ -17,10 +33,84 @@ func TestStub(t *testing.T) {
 	glc := cache.New()
 
 	assert.NotNil(t, glc)
-
 }
 
 type testHelper struct {
 	ics *istiofake.Clientset
 	ccs *certmanagerfake.Clientset
+	scs *k8sfake.Clientset
+	glc *cache.GatewayLookupCache
+}
+
+func (th *testHelper) newTestSolver() *challengesolver.ChallengeSolver {
+	return challengesolver.NewChallengeSolver(th.scs.CoreV1(), th.ics.NetworkingV1beta1(), th.ccs.AcmeV1(), th.glc)
+}
+
+func TestOne(t *testing.T) {
+	th := testHelper{
+		ics: istiofake.NewSimpleClientset(),
+		ccs: certmanagerfake.NewSimpleClientset(),
+		scs: k8sfake.NewSimpleClientset(),
+		glc: cache.New(),
+	}
+
+	th.ccs.AcmeV1().(*acmefake.FakeAcmeV1).PrependReactor(
+		"get",
+		"challenges",
+		func(action k8stesting.Action) (bool, runtime.Object, error) {
+			return true,
+				&acmev1.Challenge{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "testy",
+						Namespace: "example",
+						UID:       "12345",
+					},
+					Spec: acmev1.ChallengeSpec{
+						Token:   "testtoken",
+						DNSName: "thing.example.com",
+					},
+				}, nil
+		})
+
+	th.scs.CoreV1().(*corev1fake.FakeCoreV1).PrependReactor(
+		"list",
+		"services",
+		func(action k8stesting.Action) (bool, runtime.Object, error) {
+			return true,
+				&corev1.ServiceList{
+					Items: []corev1.Service{
+						{
+							ObjectMeta: metav1.ObjectMeta{
+								Name:      "testservice",
+								Namespace: "example",
+								Labels: map[string]string{
+									"acme.cert-manager.io/http-domain": fmt.Sprint(adler32.Checksum([]byte("thing.example.com"))),
+									"acme.cert-manager.io/http-token":  fmt.Sprint(adler32.Checksum([]byte("testtoken"))),
+								},
+							},
+							Spec: corev1.ServiceSpec{
+								Ports: []corev1.ServicePort{
+									{
+										Port: int32(8089),
+									},
+								}},
+						},
+					},
+				}, nil
+		})
+
+	cs := th.newTestSolver()
+
+	th.glc.Add("example/testgateway", "thing.example.com")
+
+	req := reconcile.Request{}
+	req.Namespace = "example"
+	req.Name = "testy"
+
+	ctx := klog.NewContext(context.Background(), zap.New(zap.Level(zapcore.DebugLevel)))
+	out, err := cs.Reconcile(ctx, req)
+
+	assert.NoError(t, err)
+	assert.NotNil(t, out)
+
 }
