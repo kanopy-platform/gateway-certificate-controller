28ebdc468fba4f8897ca15080d76ab21d4e3a0b5
David Katz <david.katz@mongodb.com>
2025-12-26 11:31:21 -0500

    adding v1 version of the controller gateway

---
+++ internal/controllers/v1/gateway/gateway.go
@@ -0,0 +1,293 @@
+package gateway
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"sort"
+	"strings"
+	"time"
+
+	"github.com/kanopy-platform/gateway-certificate-controller/pkg/v1/cache"
+	v1labels "github.com/kanopy-platform/gateway-certificate-controller/pkg/v1/labels"
+
+	certmanagerclient "github.com/cert-manager/cert-manager/pkg/client/clientset/versioned"
+
+	v1networking "istio.io/api/networking/v1"
+	networkingv1 "istio.io/client-go/pkg/apis/networking/v1"
+	istioversionedclient "istio.io/client-go/pkg/clientset/versioned"
+	istioinformers "istio.io/client-go/pkg/informers/externalversions"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	k8scache "k8s.io/client-go/tools/cache"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+
+	v1certmanager "github.com/cert-manager/cert-manager/pkg/apis/certmanager/v1"
+	v1metacertmanager "github.com/cert-manager/cert-manager/pkg/apis/meta/v1"
+)
+
+const (
+	FieldManager = "isto-cert-controller"
+)
+
+type certificateHandler interface {
+	CreateCertificate(ctx context.Context, gateway *networkingv1.Gateway, server *v1networking.Server) error
+	UpdateCertificate(ctx context.Context, cert *v1certmanager.Certificate, gateway *networkingv1.Gateway, server *v1networking.Server) error
+}
+
+type GatewayController struct {
+	istioClient          istioversionedclient.Interface
+	certClient           certmanagerclient.Interface
+	dryRun               bool
+	name                 string
+	certificateNamespace string
+	clusterIssuer        string
+	gatewayLookupCache   *cache.GatewayLookupCache
+	certHandler          certificateHandler
+	httpSolverLabel      string
+}
+
+func NewGatewayController(istioClient istioversionedclient.Interface, certClient certmanagerclient.Interface, opts ...OptionsFunc) *GatewayController {
+	gr := &GatewayController{
+		name:                 "istio-gateway-controller",
+		istioClient:          istioClient,
+		certClient:           certClient,
+		certificateNamespace: "default",
+		clusterIssuer:        "default",
+	}
+
+	for _, opt := range opts {
+		opt(gr)
+	}
+
+	gr.certHandler = gr
+	return gr
+}
+
+func (c *GatewayController) SetupWithManager(ctx context.Context, mgr manager.Manager) error {
+	log := log.FromContext(ctx)
+
+	ctrl, err := controller.New("istio-gateway-controller", mgr, controller.Options{
+		Reconciler: c,
+	})
+	if err != nil {
+		return err
+	}
+
+	istioInformerFactory := istioinformers.NewSharedInformerFactoryWithOptions(c.istioClient, time.Second*30)
+
+	informer := istioInformerFactory.Networking().V1().Gateways().Informer()
+	if c.gatewayLookupCache != nil {
+		_, err := informer.AddEventHandler(k8scache.ResourceEventHandlerFuncs{
+			AddFunc:    c.gatewayLookupCache.AddFunc,
+			UpdateFunc: c.gatewayLookupCache.UpdateFunc,
+			DeleteFunc: c.gatewayLookupCache.DeleteFunc,
+		})
+		if err != nil {
+			log.Error(err, "error adding event handler to gateway informer")
+			return err
+		}
+	}
+
+	if err := ctrl.Watch(&source.Informer{Informer: informer},
+		&handler.EnqueueRequestForObject{}); err != nil {
+		return err
+	}
+
+	istioInformerFactory.Start(ctx.Done())
+
+	return nil
+}
+
+func (c *GatewayController) Reconcile(ctx context.Context, request reconcile.Request) (reconcile.Result, error) {
+	// set up a convenient log object so we don't have to type request over and over again
+	log := log.FromContext(ctx)
+	log.Info("Reconciling Gateway...", "reconcile", request.String())
+	log.V(1).Info("Debug")
+	gateway, err := c.istioClient.NetworkingV1().Gateways(request.Namespace).Get(ctx, request.Name, metav1.GetOptions{})
+	if err != nil {
+		if errors.IsNotFound(err) {
+			return reconcile.Result{}, nil // garbage collection will handle
+		}
+
+		log.Error(err, "Error reconciling gateway, requeued")
+		return reconcile.Result{
+			Requeue: true,
+		}, err
+	}
+
+	// If we don't have the tls management label or it isn't set to true return
+	if val, ok := gateway.Labels[v1labels.InjectSimpleCredentialNameLabel]; !ok || val != "true" {
+		return reconcile.Result{}, nil
+	}
+
+	for _, s := range gateway.Spec.Servers {
+		log.V(1).Info("Inspecting server", "hosts", s.Hosts)
+
+		cert, err := c.certClient.CertmanagerV1().Certificates(c.certificateNamespace).Get(ctx, s.Tls.CredentialName, metav1.GetOptions{})
+		if err != nil {
+			if errors.IsNotFound(err) {
+				if err := c.certHandler.CreateCertificate(ctx, gateway, s); err != nil {
+					return reconcile.Result{
+						Requeue: true,
+					}, err
+				}
+			} else {
+				return reconcile.Result{
+					Requeue: true,
+				}, err
+			}
+		} else {
+			log.V(1).Info("Found certificate", "server", s)
+			if err := c.certHandler.UpdateCertificate(ctx, cert.DeepCopy(), gateway, s); err != nil {
+				return reconcile.Result{
+					Requeue: true,
+				}, err
+			}
+		}
+	}
+
+	return reconcile.Result{}, nil
+}
+
+func (c *GatewayController) CreateCertificate(ctx context.Context, gateway *networkingv1.Gateway, server *v1networking.Server) error {
+	log := log.FromContext(ctx)
+	issuer := c.clusterIssuer
+
+	if i, ok := gateway.Annotations[v1labels.IssuerAnnotation]; ok {
+		issuer = i
+	}
+
+	if server.Tls.Mode != v1networking.ServerTLSSettings_SIMPLE {
+		return nil
+	}
+
+	cert := &v1certmanager.Certificate{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Certificate",
+			APIVersion: "cert-manager.io/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        server.Tls.CredentialName,
+			Labels:      map[string]string{v1labels.ManagedLabel: fmt.Sprintf("%s.%s", gateway.Name, gateway.Namespace)},
+			Annotations: map[string]string{},
+		},
+		Spec: v1certmanager.CertificateSpec{
+			DNSNames:   getSortedHostsWithoutNamespace(server.Hosts),
+			SecretName: server.Tls.CredentialName,
+			IssuerRef: v1metacertmanager.ObjectReference{
+				Kind:  "ClusterIssuer",
+				Name:  issuer,
+				Group: "cert-manager.io",
+			},
+		},
+	}
+
+	if b, ok := gateway.Annotations[v1labels.IssueTemporaryCertificateAnnotation]; ok && b == "true" {
+		cert.Annotations[v1certmanager.IssueTemporaryCertificateAnnotation] = "true"
+	}
+
+	if b, ok := gateway.Annotations[v1labels.HTTPSolverAnnotation]; ok && b == "true" {
+		cert.Labels[c.httpSolverLabel] = "true"
+	}
+	createOptions := metav1.CreateOptions{FieldManager: FieldManager}
+	if c.dryRun {
+		log.Info("[dryrun] create certificate", "cert", cert)
+		createOptions.DryRun = []string{metav1.DryRunAll}
+	}
+	_, err := c.certClient.CertmanagerV1().Certificates(c.certificateNamespace).Create(ctx, cert, createOptions)
+	return err
+}
+
+func getSortedHostsWithoutNamespace(serverHosts []string) []string {
+	hosts := make([]string, len(serverHosts))
+
+	for i, h := range serverHosts {
+		parts := strings.Split(h, "/")
+		hosts[i] = h
+		if len(parts) > 1 {
+			hosts[i] = parts[1]
+		}
+	}
+	sort.Strings(hosts)
+	return hosts
+}
+
+func (c *GatewayController) UpdateCertificate(ctx context.Context, cert *v1certmanager.Certificate, gateway *networkingv1.Gateway, server *v1networking.Server) error {
+	log := log.FromContext(ctx)
+
+	cert, updatedIssuer := updateCertificateIssuer(ctx, cert, gateway)
+	cert, updatedDNSNames := updateCertificateDNSNames(ctx, cert, server)
+	cert, updatedHTTPSolver := updateHTTPSolver(ctx, cert, gateway, c.httpSolverLabel)
+
+	if updatedDNSNames || updatedIssuer || updatedHTTPSolver {
+		log.V(1).Info("pre-update", "cert", cert)
+
+		updateOptions := metav1.UpdateOptions{FieldManager: FieldManager}
+		if c.dryRun {
+			log.Info("[dryrun] update certificate", "cert", cert)
+			updateOptions.DryRun = []string{metav1.DryRunAll}
+		}
+
+		_, err := c.certClient.CertmanagerV1().Certificates(c.certificateNamespace).Update(ctx, cert, updateOptions)
+		if err != nil {
+			log.Error(err, "error on certificate update", "cert", cert)
+		}
+		return err
+	}
+
+	return nil
+}
+
+func updateHTTPSolver(ctx context.Context, cert *v1certmanager.Certificate, gateway *networkingv1.Gateway, label string) (*v1certmanager.Certificate, bool) {
+	log := log.FromContext(ctx)
+
+	if h, ok := gateway.Annotations[v1labels.HTTPSolverAnnotation]; ok && h == "true" {
+		if cert.Labels == nil {
+			cert.Labels = map[string]string{}
+		}
+
+		if l, ok := cert.Labels[label]; !ok || (ok && l != "true") {
+			log.V(1).Info("Adding http solver label to cert")
+			cert.Labels[label] = "true"
+			return cert, true
+		} else {
+			return cert, false
+		}
+	}
+
+	if l, ok := cert.Labels[label]; ok && l == "true" {
+		log.V(1).Info("Removing http solver label from cert")
+		delete(cert.Labels, label)
+		return cert, true
+	}
+
+	return cert, false
+}
+
+func updateCertificateIssuer(ctx context.Context, cert *v1certmanager.Certificate, gateway *networkingv1.Gateway) (*v1certmanager.Certificate, bool) {
+	log := log.FromContext(ctx)
+	issuer := cert.Spec.IssuerRef.Name
+
+	if i, ok := gateway.Annotations[v1labels.IssuerAnnotation]; ok {
+		log.V(1).Info("got issuer from annotation", "issuer", i)
+		issuer = i
+	}
+
+	updated := cert.Spec.IssuerRef.Name != issuer
+	cert.Spec.IssuerRef.Name = issuer
+	return cert, updated
+}
+
+func updateCertificateDNSNames(ctx context.Context, cert *v1certmanager.Certificate, server *v1networking.Server) (*v1certmanager.Certificate, bool) {
+	hosts := getSortedHostsWithoutNamespace(server.Hosts)
+	updated := !reflect.DeepEqual(hosts, cert.Spec.DNSNames)
+	cert.Spec.DNSNames = hosts
+	return cert, updated
+}
---
+++ internal/controllers/v1/gateway/gateway_test.go
@@ -0,0 +1,508 @@
+package gateway
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+
+	certmanagerfake "github.com/cert-manager/cert-manager/pkg/client/clientset/versioned/fake"
+	certmanagerv1fake "github.com/cert-manager/cert-manager/pkg/client/clientset/versioned/typed/certmanager/v1/fake"
+	"github.com/kanopy-platform/gateway-certificate-controller/pkg/v1/cache"
+	v1beta1labels "github.com/kanopy-platform/gateway-certificate-controller/pkg/v1/labels"
+
+	networkingv1 "istio.io/api/networking/v1"
+
+	v1 "istio.io/client-go/pkg/apis/networking/v1"
+	networkingv1fake "istio.io/client-go/pkg/clientset/versioned/typed/networking/v1/fake"
+
+	istiofake "istio.io/client-go/pkg/clientset/versioned/fake"
+	k8stesting "k8s.io/client-go/testing"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	v1certmanager "github.com/cert-manager/cert-manager/pkg/apis/certmanager/v1"
+	v1certmanagermeta "github.com/cert-manager/cert-manager/pkg/apis/meta/v1"
+)
+
+const (
+	TestNamespace       = "test"
+	TestGatewayName     = "mygateway"
+	TestCertificateName = "mygateway-cert"
+	TestCertNamespace   = "certnamespace"
+)
+
+type controllerSpy struct {
+	*GatewayController
+	CleanupCalled int
+	CreateCalled  int
+	UpdateCalled  int
+	Error         bool
+}
+
+type GatewayOptions struct {
+	Certificates       []runtime.Object
+	Hosts              []string
+	CredentialName     string
+	DryRun             bool
+	GatewayLookupCache *cache.GatewayLookupCache
+	Annotations        map[string]string
+	Labels             map[string]string
+	Servers            []*networkingv1.Server
+}
+
+func namespacedHost(host string) string {
+	return fmt.Sprintf("%s/%s", TestNamespace, host)
+}
+
+func namespacedHosts(hosts ...string) []string {
+	nh := make([]string, len(hosts))
+	for i, h := range hosts {
+		nh[i] = namespacedHost(h)
+	}
+	return nh
+}
+
+func WithCredentialName(name string) func(*GatewayOptions) {
+	return func(gopt *GatewayOptions) {
+		gopt.CredentialName = name
+	}
+}
+
+func WithTestDryRun() func(*GatewayOptions) {
+	return func(gopt *GatewayOptions) {
+		gopt.DryRun = true
+	}
+}
+
+func WithAnnotations(annotations map[string]string) func(*GatewayOptions) {
+	return func(gopt *GatewayOptions) {
+		gopt.Annotations = annotations
+	}
+}
+
+func WithLabels(labels map[string]string) func(*GatewayOptions) {
+	return func(gopt *GatewayOptions) {
+		gopt.Labels = labels
+	}
+}
+
+func WithGLC(glc *cache.GatewayLookupCache) func(*GatewayOptions) {
+	return func(gopt *GatewayOptions) {
+		gopt.GatewayLookupCache = glc
+	}
+}
+
+func AppendServer(server *networkingv1.Server) func(*GatewayOptions) {
+	return func(gopt *GatewayOptions) {
+		gopt.Servers = append(gopt.Servers, server)
+	}
+}
+
+func AppendHosts(host string) func(*GatewayOptions) {
+	return func(gopt *GatewayOptions) {
+		gopt.Hosts = append(gopt.Hosts, namespacedHost(host))
+	}
+}
+
+func WithHosts(hosts ...string) func(*GatewayOptions) {
+	return func(gopt *GatewayOptions) {
+		gopt.Hosts = namespacedHosts(hosts...)
+	}
+}
+
+func AppendCertificates(o ...runtime.Object) func(*GatewayOptions) {
+	return func(gopt *GatewayOptions) {
+		gopt.Certificates = append(gopt.Certificates, o...)
+	}
+}
+
+func NewGatewayOptions(opts ...func(*GatewayOptions)) *GatewayOptions {
+	gopts := &GatewayOptions{
+		Hosts:          append(namespacedHosts("test2.example.com"), "test1.example.com"),
+		CredentialName: TestCertificateName,
+		Labels:         map[string]string{v1beta1labels.InjectSimpleCredentialNameLabel: "true"},
+	}
+
+	for _, o := range opts {
+		o(gopts)
+	}
+
+	return gopts
+}
+
+func gatewayListAction(opts ...func(*GatewayOptions)) func(k8stesting.Action) (bool, runtime.Object, error) {
+	gopts := NewGatewayOptions(opts...)
+	return func(action k8stesting.Action) (bool, runtime.Object, error) {
+		servers := []*networkingv1.Server{
+			{
+				Hosts: gopts.Hosts,
+				Tls: &networkingv1.ServerTLSSettings{
+					CredentialName: gopts.CredentialName,
+					Mode:           networkingv1.ServerTLSSettings_SIMPLE,
+				},
+			},
+		}
+
+		servers = append(servers, gopts.Servers...)
+
+		return true, &v1.Gateway{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "Gateway",
+				APIVersion: "networking.istio.io/v1beta1",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:        TestGatewayName,
+				Namespace:   TestNamespace,
+				Annotations: gopts.Annotations,
+				Labels:      gopts.Labels,
+			},
+			Spec: networkingv1.Gateway{
+				Servers: servers,
+			},
+		}, nil
+	}
+}
+
+type TestHelper struct {
+	IstioClient *istiofake.Clientset
+	CertClient  *certmanagerfake.Clientset
+	Controller  *controllerSpy
+}
+
+func NewTestHelper(opts *GatewayOptions) *TestHelper {
+	ics := istiofake.NewSimpleClientset()
+	ccs := certmanagerfake.NewSimpleClientset(opts.Certificates...)
+	return &TestHelper{
+		IstioClient: ics,
+		CertClient:  ccs,
+		Controller:  setupControllerWithSpy(ics, ccs, opts),
+	}
+}
+
+func NewTestHelperWithGateways(opts ...func(*GatewayOptions)) *TestHelper {
+	gopts := NewGatewayOptions(opts...)
+	helper := NewTestHelper(gopts)
+
+	helper.IstioClient.NetworkingV1().(*networkingv1fake.FakeNetworkingV1).PrependReactor(
+		"get",
+		"gateways",
+		gatewayListAction(opts...),
+	)
+
+	return helper
+}
+
+func NewTestHelperWithCertificates(opts ...func(*GatewayOptions)) *TestHelper {
+	opts = append(opts, AppendCertificates(&v1certmanager.Certificate{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Certificate",
+			APIVersion: "cert-manager.io/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      TestCertificateName,
+			Namespace: TestCertNamespace,
+		},
+		Spec: v1certmanager.CertificateSpec{
+			DNSNames: []string{"test1.example.com", "test2.example.com"},
+			IssuerRef: v1certmanagermeta.ObjectReference{
+				Kind:  "ClusterIssuer",
+				Name:  "default",
+				Group: "cert-manager.io",
+			},
+		},
+	}))
+
+	helper := NewTestHelperWithGateways(opts...)
+	return helper
+}
+
+func setupControllerWithSpy(cs *istiofake.Clientset, certFake *certmanagerfake.Clientset, opts *GatewayOptions) *controllerSpy {
+	spy := &controllerSpy{
+		GatewayController: NewGatewayController(cs, certFake, WithDryRun(opts.DryRun), WithCertificateNamespace(TestCertNamespace), WithGatewayLookupCache(opts.GatewayLookupCache), WithHTTPSolverLabel("use-istio-http01-solver")),
+	}
+	spy.certHandler = spy
+	return spy
+}
+
+func (r *controllerSpy) CreateCertificate(ctx context.Context, gateway *v1.Gateway, server *networkingv1.Server) error {
+	r.CreateCalled++
+	if r.Error {
+		return fmt.Errorf("mock create error")
+	}
+	return r.GatewayController.CreateCertificate(ctx, gateway, server)
+}
+
+func (r *controllerSpy) UpdateCertificate(ctx context.Context, cert *v1certmanager.Certificate, gateway *v1.Gateway, server *networkingv1.Server) error {
+	r.UpdateCalled++
+	if r.Error {
+		return fmt.Errorf("mock update error")
+	}
+	return r.GatewayController.UpdateCertificate(ctx, cert, gateway, server)
+}
+
+func reconcileRequest() reconcile.Request {
+	return reconcile.Request{
+		NamespacedName: types.NamespacedName{
+			Namespace: TestNamespace,
+			Name:      TestGatewayName,
+		},
+	}
+}
+
+func assertCreateCertificateCalled(t *testing.T, helper *TestHelper) {
+	r, err := helper.Controller.Reconcile(context.TODO(), reconcileRequest())
+	assert.NoError(t, err)
+	assert.Equal(t, 1, helper.Controller.CreateCalled)
+	assert.Equal(t, reconcile.Result{}, r)
+}
+
+func assertCertificateUpdated(t *testing.T, helper *TestHelper) {
+	_, err := helper.Controller.Reconcile(context.TODO(), reconcileRequest())
+	assert.NoError(t, err)
+	assert.Equal(t, 0, helper.Controller.CreateCalled)
+	assert.Equal(t, 1, helper.Controller.UpdateCalled)
+}
+
+func TestGatewayControllerReconcileNoError(t *testing.T) {
+	t.Parallel()
+	g := NewGatewayController(istiofake.NewSimpleClientset(), certmanagerfake.NewSimpleClientset(), WithCertificateNamespace("default"))
+	r, err := g.Reconcile(context.TODO(), reconcile.Request{})
+	assert.NoError(t, err)
+	assert.Equal(t, reconcile.Result{}, r)
+}
+
+func TestGatewayReconcile_OkOnNotExists(t *testing.T) {
+	t.Parallel()
+	helper := NewTestHelper(NewGatewayOptions())
+	r, err := helper.Controller.Reconcile(context.TODO(), reconcileRequest())
+	assert.NoError(t, err)
+	assert.Equal(t, reconcile.Result{}, r)
+}
+
+func TestGatewyReconcile_CreatesCertificateForGateway(t *testing.T) {
+	t.Parallel()
+	helper := NewTestHelperWithGateways()
+	assertCreateCertificateCalled(t, helper)
+	certList, err := helper.CertClient.CertmanagerV1().Certificates(TestCertNamespace).List(context.TODO(), metav1.ListOptions{})
+	assert.NoError(t, err)
+	assert.Len(t, certList.Items, 1)
+}
+
+func TestGatewayReconcile_CallsCreateCertificateWithError(t *testing.T) {
+	t.Parallel()
+	helper := NewTestHelperWithGateways()
+	helper.Controller.Error = true
+	r, err := helper.Controller.Reconcile(context.TODO(), reconcileRequest())
+	assert.Error(t, err)
+	assert.Equal(t, reconcile.Result{Requeue: true}, r)
+}
+
+func TestGatewayReconcile_CreateCertificateUsingCredentialName(t *testing.T) {
+	t.Parallel()
+	helper := NewTestHelperWithGateways()
+	assertCreateCertificateCalled(t, helper)
+	cert, err := helper.CertClient.CertmanagerV1().Certificates(TestCertNamespace).Get(context.TODO(), TestCertificateName, metav1.GetOptions{})
+	assert.NoError(t, err)
+	assert.NotNil(t, cert)
+}
+
+func TestGatewayReconcile_CreateCertificateLabeledAsManaged(t *testing.T) {
+	t.Parallel()
+	helper := NewTestHelperWithGateways()
+	assertCreateCertificateCalled(t, helper)
+	cert, err := helper.CertClient.CertmanagerV1().Certificates(TestCertNamespace).Get(context.TODO(), TestCertificateName, metav1.GetOptions{})
+	assert.NoError(t, err)
+	assert.NotNil(t, cert)
+	assert.Equal(t, fmt.Sprintf("%s.%s", TestGatewayName, TestNamespace), cert.Labels[v1beta1labels.ManagedLabel])
+}
+
+func TestGatewayReconcile_CreateCertificateWithHosts(t *testing.T) {
+	t.Parallel()
+	glc := cache.New()
+	helper := NewTestHelperWithGateways(WithGLC(glc))
+	assertCreateCertificateCalled(t, helper)
+	cert, err := helper.CertClient.CertmanagerV1().Certificates(TestCertNamespace).Get(context.TODO(), TestCertificateName, metav1.GetOptions{})
+	assert.NoError(t, err)
+	assert.Equal(t, []string{"test1.example.com", "test2.example.com"}, cert.Spec.DNSNames)
+}
+
+func TestGatewayReconcile_CreatCertificateWithDefaultIssuer(t *testing.T) {
+	t.Parallel()
+	helper := NewTestHelperWithGateways()
+	assertCreateCertificateCalled(t, helper)
+	cert, err := helper.CertClient.CertmanagerV1().Certificates(TestCertNamespace).Get(context.TODO(), TestCertificateName, metav1.GetOptions{})
+	assert.NoError(t, err)
+	assert.Equal(t, "default", cert.Spec.IssuerRef.Name)
+	assert.Equal(t, "ClusterIssuer", cert.Spec.IssuerRef.Kind)
+}
+
+func TestGatewayReconcile_CreatCertificateWithClusterIssuerFromGatewayAnnotation(t *testing.T) {
+	t.Parallel()
+	helper := NewTestHelperWithGateways(WithAnnotations(map[string]string{
+		v1beta1labels.IssuerAnnotation: "testissuer",
+	}))
+	assertCreateCertificateCalled(t, helper)
+	cert, err := helper.CertClient.CertmanagerV1().Certificates(TestCertNamespace).Get(context.TODO(), TestCertificateName, metav1.GetOptions{})
+	assert.NoError(t, err)
+	assert.Equal(t, "testissuer", cert.Spec.IssuerRef.Name)
+	assert.Equal(t, "ClusterIssuer", cert.Spec.IssuerRef.Kind)
+}
+
+func TestGatewayReconcile_CreateCertificateWithTempCertAnnotation(t *testing.T) {
+	t.Parallel()
+	helper := NewTestHelperWithGateways(WithAnnotations(map[string]string{
+		v1beta1labels.IssueTemporaryCertificateAnnotation: "true",
+	}))
+	assertCreateCertificateCalled(t, helper)
+	cert, err := helper.CertClient.CertmanagerV1().Certificates(TestCertNamespace).Get(context.TODO(), TestCertificateName, metav1.GetOptions{})
+	assert.NoError(t, err)
+	out, ok := cert.Annotations[v1certmanager.IssueTemporaryCertificateAnnotation]
+	assert.True(t, ok)
+	assert.Equal(t, "true", out)
+}
+
+func TestGatewayReconcile_SkipCertificateForTLSModePassthrough(t *testing.T) {
+	t.Parallel()
+	helper := NewTestHelperWithGateways(AppendServer(&networkingv1.Server{
+		Hosts: []string{"pass.example.com"},
+		Tls: &networkingv1.ServerTLSSettings{
+			Mode: networkingv1.ServerTLSSettings_AUTO_PASSTHROUGH,
+		},
+	}))
+	_, err := helper.Controller.Reconcile(context.TODO(), reconcileRequest())
+	assert.NoError(t, err)
+	certList, err := helper.CertClient.CertmanagerV1().Certificates(TestCertNamespace).List(context.TODO(), metav1.ListOptions{})
+	assert.NoError(t, err)
+	assert.Len(t, certList.Items, 1)
+	assert.Equal(t, []string{"test1.example.com", "test2.example.com"}, certList.Items[0].Spec.DNSNames)
+}
+
+func TestGatewayReconcile_CallsUpdateCertificateWhenCertificateExists(t *testing.T) {
+	t.Parallel()
+	helper := NewTestHelperWithCertificates()
+	assertCertificateUpdated(t, helper)
+}
+
+func TestGatewayReconcile_UpdatesCertificateWithNewHost(t *testing.T) {
+	t.Parallel()
+	helper := NewTestHelperWithCertificates(AppendHosts("an.example.com"))
+	assertCertificateUpdated(t, helper)
+	cert, err := helper.CertClient.CertmanagerV1().Certificates(TestCertNamespace).Get(context.TODO(), TestCertificateName, metav1.GetOptions{})
+	assert.NoError(t, err)
+	assert.Equal(t, []string{"an.example.com", "test1.example.com", "test2.example.com"}, cert.Spec.DNSNames)
+}
+
+func TestGatewayReconcile_UpdatesCertificateWithDeletedHost(t *testing.T) {
+	t.Parallel()
+	helper := NewTestHelperWithCertificates(WithHosts("test2.example.com"))
+	assertCertificateUpdated(t, helper)
+	cert, err := helper.CertClient.CertmanagerV1().Certificates(TestCertNamespace).Get(context.TODO(), TestCertificateName, metav1.GetOptions{})
+	assert.NoError(t, err)
+	assert.Equal(t, []string{"test2.example.com"}, cert.Spec.DNSNames)
+}
+
+func TestGatewayReconcile_UpdatesCertificateWithNewIssuer(t *testing.T) {
+	t.Parallel()
+	helper := NewTestHelperWithCertificates(WithAnnotations(map[string]string{
+		v1beta1labels.IssuerAnnotation: "new",
+	}))
+	assertCertificateUpdated(t, helper)
+	cert, err := helper.CertClient.CertmanagerV1().Certificates(TestCertNamespace).Get(context.TODO(), TestCertificateName, metav1.GetOptions{})
+	assert.NoError(t, err)
+	assert.Equal(t, "new", cert.Spec.IssuerRef.Name)
+	assert.Equal(t, "ClusterIssuer", cert.Spec.IssuerRef.Kind)
+}
+
+func TestGatewayReconcile_UpdatesCertificateWithSolver(t *testing.T) {
+	helper := NewTestHelperWithCertificates(WithAnnotations(map[string]string{v1beta1labels.HTTPSolverAnnotation: "true"}))
+	assertCertificateUpdated(t, helper)
+	cert, err := helper.CertClient.CertmanagerV1().Certificates(TestCertNamespace).Get(context.TODO(), TestCertificateName, metav1.GetOptions{})
+	assert.NoError(t, err)
+
+	l, ok := cert.Labels["use-istio-http01-solver"]
+	assert.True(t, ok)
+	assert.Equal(t, "true", l)
+}
+
+func TestGatewayReconcile_DeleteCertificateSolver(t *testing.T) {
+	helper := NewTestHelperWithGateways(AppendCertificates(
+		&v1certmanager.Certificate{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "Certificate",
+				APIVersion: "cert-manager.io/v1",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      TestCertificateName,
+				Namespace: TestCertNamespace,
+				Labels: map[string]string{
+					"use-istio-http01-solver": "true",
+				},
+			},
+			Spec: v1certmanager.CertificateSpec{
+				DNSNames: []string{"test1.example.com", "test2.example.com"},
+				IssuerRef: v1certmanagermeta.ObjectReference{
+					Kind:  "ClusterIssuer",
+					Name:  "default",
+					Group: "cert-manager.io",
+				},
+			},
+		},
+	))
+	assertCertificateUpdated(t, helper)
+	cert, err := helper.CertClient.CertmanagerV1().Certificates(TestCertNamespace).Get(context.TODO(), TestCertificateName, metav1.GetOptions{})
+	assert.NoError(t, err)
+
+	l, ok := cert.Labels["use-istio-http01-solver"]
+	assert.False(t, ok)
+	assert.Equal(t, "", l)
+}
+
+func TestGatewayReconcile_UpdateCertificateNoOp(t *testing.T) {
+	t.Parallel()
+	helper := NewTestHelperWithCertificates()
+	updated := 0
+	helper.CertClient.CertmanagerV1().(*certmanagerv1fake.FakeCertmanagerV1).PrependReactor("update", "certificates", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {
+		updated++
+		return true, nil, nil
+	})
+
+	assertCertificateUpdated(t, helper)
+	assert.Equal(t, 0, updated)
+}
+
+func TestGatewayReconcile_SkipGatewayWithoutLabel(t *testing.T) {
+	t.Parallel()
+	helper := NewTestHelperWithGateways(WithLabels(map[string]string{}))
+
+	assert.Equal(t, 0, helper.Controller.CreateCalled)
+	cert, err := helper.CertClient.CertmanagerV1().Certificates(TestCertNamespace).Get(context.TODO(), TestCertificateName, metav1.GetOptions{})
+	assert.Error(t, err)
+	assert.Nil(t, cert)
+}
+
+func TestHTTPSolverLabelIdempotency(t *testing.T) {
+	t.Parallel()
+	cert := &v1certmanager.Certificate{
+		ObjectMeta: metav1.ObjectMeta{
+			Labels: map[string]string{},
+		},
+	}
+	gw := &v1.Gateway{
+		ObjectMeta: metav1.ObjectMeta{
+			Annotations: map[string]string{v1beta1labels.HTTPSolverAnnotation: "true"},
+		},
+	}
+	// Add the label
+	cert, updated := updateHTTPSolver(context.Background(), cert, gw, "use-istio-http01-solver")
+	assert.True(t, updated)
+	assert.Equal(t, "true", cert.Labels["use-istio-http01-solver"])
+
+	// Test for idempotency
+	cert, updated = updateHTTPSolver(context.Background(), cert, gw, "use-istio-http01-solver")
+	assert.False(t, updated)
+	assert.Equal(t, "true", cert.Labels["use-istio-http01-solver"])
+}
